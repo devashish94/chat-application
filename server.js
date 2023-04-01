76caab6d98cdf6723fedaedd9c900f1e633432c0
devashish94 <roy.devashish94@gmail.com>
2023-04-01 22:58:36 +0530

    add more front-end

--- server.js
+++ server.js
@@ -5,14 +5,23 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 const HOST = 'localhost';
 
-app.use(express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/public'));
 
 const server = app.listen(3000, () => console.log(`Server has started at http://localhost:${PORT}`));
 
-app.get('/', (req, res) => {
-    res.send('User Here')
-});
+const io = require('socket.io')(server);
 
-app.post('/chat-application.html', (req, res) => {
-    res.send('<h1>This is chatting<h1>')
-})
+clientsConnected = [];
+
+// listen for socket.io connections
+io.on('connection', socket => {
+    console.log(`socket connection with id: ${socket.id}`);
+    clientsConnected[socket.id] = '1';
+
+    // emit user connected message to all connected clients
+
+    // handle the socket client disconnect event
+    socket.on('disconnect', () => {
+        console.log(`socket id: ${socket.id} has disconnected`);
+    });
+});
