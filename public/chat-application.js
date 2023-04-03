3c848365b5475f40e1a4cb180b86063463f19104
devashish94 <roy.devashish94@gmail.com>
2023-04-03 13:46:57 +0530

    add basic communication

--- public/chat-application.js
+++ public/chat-application.js
@@ -1,1 +1,36 @@
 const socket = io();
+
+const mainChat = document.getElementById('main-chat');
+const chatInput = document.getElementById('chat-input');
+const sendButton = document.getElementById('send-button');
+let userID = 69;
+
+socket.on('admin-message', message => {
+    console.log(`${message.id}: ${message.chatMessage}`);
+    userID = message.id;
+});
+
+socket.on('new-message', message => {
+    console.log(`${message.id}: ${message.chatMessage}`);
+    const chat = `${socket.id}: ${message.chatMessage}`;
+    const newDiv = newDivMessage(chat);
+    mainChat.appendChild(newDiv);
+});
+
+sendButton.onclick = () => {
+    const inputText= chatInput.value;
+    const newChat = `You: ${inputText}`;
+    const newDiv = newDivMessage(newChat);  
+    mainChat.appendChild(newDiv);
+    socket.emit('new-message', {id: userID, chatMessage: inputText});
+};
+
+function newDivMessage(chatText) {
+    const newDiv = document.createElement('div');
+    newDiv.innerText = chatText;
+    newDiv.style = 'border-radius: 7px; margin: 10px; border: 1px solid black';
+    return newDiv;
+};
+
+
+// use URL parser to extract the username from URL
--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 42069;
 const HOST = 'localhost';
 
 app.use('/', express.static(__dirname + '/public'));
 
-const server = app.listen(3000, () => console.log(`Server has started at http://localhost:${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server has started at http://localhost:${PORT}`));
 
 const io = require('socket.io')(server);
 
@@ -17,12 +17,20 @@ clientsConnected = [];
 io.on('connection', socket => {
     console.log(`socket connection with id: ${socket.id}`);
     clientsConnected[socket.id] = '1';
+    socket.emit('admin-message', {id: socket.id, chatMessage: 'Welcome to the Common Conversations Room'});
+
+    // send user joined message to everyone other than the one joined
+    socket.broadcast.emit('admin-message', {id: socket.id, chatMessage: 'has joined the chat'});
+
+    // message from user
+    socket.on('new-message', message => {
+        console.log(`${message.id}: ${message.chatMessage}`);
+        socket.broadcast.emit('new-message', message);
+    });
     
-
-    // emit user connected message to all connected clients
-
     // handle the socket client disconnect event
     socket.on('disconnect', () => {
         console.log(`socket id: ${socket.id} has disconnected`);
+        io.emit('admin-message', {id: socket.id, chatMessage: 'has left the chat'});
     });
 });
